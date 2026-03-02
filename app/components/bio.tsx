8f98d197185b972c56a2145a5c732fc4b30077ae
Parineet <psond@uic.edu>
2026-03-01 19:49:59 -0600

    added a simple component with header, paragraph, image and div tag

---
+++ app/components/bio.tsx
@@ -0,0 +1,11 @@
+export default function Bio(){
+    return(
+        <div>
+            <h1>Parineet Sond</h1>
+            <p>Computer Science Honors student graduating May 2028.
+            Based in Chicago, IL 60607.
+            </p>
+            <img src = "profilePic.jpg" alt = "Profile Picture" width = "200"/>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import Bio from "./components/bio";
 
 export default function Home() {
   return (
-    <div className="flex flex-row gap-4">
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
-      <Link href="/projects">Projects</Link>
+    <div>
+      <div className="flex flex-row gap-4">
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/projects">Projects</Link>
+      </div>
+    
+      <Bio />
     </div>
   );
 }
