4a7ac54ef6a728422337493b7d8bcfd029127fd7
Parineet <psond@uic.edu>
2026-03-16 01:25:07 -0500

    have only the req. images on homepage

--- app/components/homeCollage.tsx
+++ app/components/homeCollage.tsx
@@ -1,91 +1,84 @@
 import Image from "next/image";
 
 export default function HomeCollage(){
-    return(
-        <div className = "flex justify-center">
-        <div className="relative w-[900px] h-[400px] bg-gray-200">
-          <Image
-            src="/CU1.png"
-            alt="picture of CU1"
-            width={300}
-            height={200}
-            className="absolute top-0 left-0 rotate-[-6deg]"
-          />
-          
+  return(
+    <div className = "flex justify-center">
+      <div className="relative w-screen h-[800px] bg-gray-200">
+        <Image
+          src="/CU1.png"
+          alt="picture of CU1"
+          width={300}
+          height={200}
+          className="absolute top-20 left-30"
+        />
+      
+        <Image
+          src="/mentor.png"
+          alt="picture of mentor slide"
+          width={300}
+          height={200}
+          className="absolute top-24 left-1 "
+        />
 
-          <Image
-            src="/FHTC2.png"
-            alt="picture of FHTC project"
-            width={300}
-            height={200}
-            className="absolute top-12 left-24 rotate-[8deg]"
-          />
 
-          <Image
-            src="/javascript.png"
-            alt="picture of JS logo"
-            width={150}
-            height={200}
-            className="absolute top-24 left-1 rotate-[-3deg]"
-          />
 
-          <Image
-            src="/mentor.png"
-            alt="picture of mentor slide"
-            width={300}
-            height={200}
-            className="absolute top-24 left-1 rotate-[-3deg]"
-          />
+        <Image
+          src="/shatterMe.png"
+          alt="picture of mentor slide"
+          width={100}
+          height={200}
+          className="absolute top-24 left-1 "
+        />
 
-          <Image
-            src="/pinterest.png"
-            alt="picture of pinterest logo"
-            width={100}
-            height={200}
-            className="absolute top-24 left-1 rotate-[-3deg]"
-          />
-
-          <Image
-            src="/react.png"
-            alt="picture of react logo"
-            width={300}
-            height={200}
-            className="absolute top-24 left-1 rotate-[-3deg]"
-          />
-
-          <Image
-            src="/shatterMe.png"
-            alt="picture of mentor slide"
-            width={100}
-            height={200}
-            className="absolute top-24 left-1 rotate-[-3deg]"
-          />
-
+        <div className="absolute top-[40%] left-[30%] z-30 rotate-[10deg]">
           <Image
             src="/sweaterWeather.png"
-            alt="picture of sweater Weather by The Neighbourhood"
-            width={100}
-            height={200}
-            className="absolute top-25 left-150 rotate-[5deg] rounded-lg"
+            alt="Spotify Player"
+            width={150} 
+            height={150}
+            className="rounded-xl shadow-2xl"
           />
-
-          <Image
-            src="/Sweet-T thumbnail.png"
-            alt="picture of Sweet-T website thumbnail"
-            width={300}
-            height={200}
-            className="absolute top-24 left-1 rotate-[-3deg]"
-          />
-
-          {/* <Image
-            src="/Sweet-T2.png"
-            alt="picture of Sweet-T website thumbnail"
-            width={300}
-            height={200}
-            className="absolute top-24 left-1 rotate-[-3deg]"
-          /> */}
         </div>
-      </div>
 
-    );
+        <Image
+          src="/Sweet-T thumbnail.png"
+          alt="picture of Sweet-T website thumbnail"
+          width={300}
+          height={200}
+          className="absolute top-24 left-1"
+        />
+
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
+            <Image
+              src="/aboutMe1.png"
+              alt="picture of me (main)"
+              width={340} // Adjust based on your preference
+              height={480}
+              className="rounded-[40px] shadow-2xl"
+            />
+          </div>
+
+        <Image
+          src="/motorola.jpg"
+          alt="picture of me at mototeola"
+          width={300}
+          height={200}
+          className="absolute top-24 left-1"
+        />
+
+        <div className="absolute top-[40%] left-[20%] z-10 ">
+          <Image
+            src="/SparkHacks.png"
+            alt="picture of me at sparkHacks"
+            width={300}
+            height={200}
+            className="rounded-2xl"
+          />
+        </div>
+
+    
+      </div>
+    </div>
+
+  );
 }
