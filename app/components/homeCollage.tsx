689973ddf64e2db97537197eb4176510e7386fd3
Parineet <psond@uic.edu>
2026-03-28 12:54:45 -0500

    home collage image hover transition + text border added

--- app/components/homeCollage.tsx
+++ app/components/homeCollage.tsx
@@ -5,91 +5,91 @@ export default function HomeCollage(){
     <div className = "flex justify-center">
       <div className="relative w-screen h-[800px]">
 
-        <div className = "absolute top-[55%] left-[-4%]">
+        <div className = "absolute top-[55%] left-[-4%] group">
           <Image
             src="/CU1.png"
             alt="picture of CU1"
             width={350}
             height={200}
-            className="rounded-xl shadow-2xl"
+            className="rounded-xl shadow-2xl transition-transform duration-300 ease-out hover:scale-110 hover:-translate-y-2 hover:shadow-3xl group-hover:rotate-2"
           />
         </div>
         
-        <div className = "absolute top-[23%] -right-30 z-50 -rotate-6">
+        <div className = "absolute top-[23%] -right-30 z-50 -rotate-6 group">
           <Image
             src="/mentor.png"
             alt="picture of mentor slide"
             width={320}
             height={200}
-            className="rounded-xl shadow-2xl"
+            className="rounded-xl shadow-2xl transition-transform duration-300 ease-out hover:scale-110 hover:-translate-y-2 hover:shadow-3xl group-hover:rotate-2"
           />
         </div>
 
 
-        <div className = "absolute top-[50%] left-[11%] z-30">
+        <div className = "absolute top-[50%] left-[11%] z-30 group">
           <Image
             src="/shatterMe.png"
             alt="picture of fav book"
             width={120}
             height={200}
-            className="rounded-xl shadow-2xl"
+            className="rounded-xl shadow-2xl transition-transform duration-300 ease-out hover:scale-110 hover:-translate-y-2 hover:shadow-3xl group-hover:rotate-2"
           />
         </div>
 
-        <div className="absolute top-[28%] left-[30%] z-30 rotate-[10deg]">
+        <div className="absolute top-[28%] left-[30%] z-30 rotate-[10deg] group">
           <Image
             src="/sweaterWeather.png"
             alt="fav song"
             width={135} 
             height={150}
-            className="rounded-xl shadow-2xl"
+            className="rounded-xl shadow-2xl transition-transform duration-300 ease-out hover:scale-110 hover:-translate-y-2 hover:shadow-3xl group-hover:rotate-2"
           />
         </div>
 
   
 
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -translate-y-[70%] z-20">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -translate-y-[70%] z-20 group">
           <Image
             src="/aboutMe1.png"
             alt="picture of me (main)"
             width={430} 
             height={480}
-            className="rounded-[40px] shadow-2xl"
+            className="rounded-[40px] shadow-2xl transition-transform duration-300 ease-out hover:scale-110 hover:-translate-y-2 hover:shadow-3xl group-hover:rotate-2"
           />
         </div>
 
-        <div className = "absolute top-[40%] right-0 z-40">
+        <div className = "absolute top-[40%] right-0 z-40 group">
           <Image
             src="/motorola.png"
             alt="picture of me at motorola"
             width={230}
             height={200}
-            className="rounded-[40px] shadow-2xl"
+            className="rounded-[40px] shadow-2xl transition-transform duration-300 ease-out hover:scale-110 hover:-translate-y-2 hover:shadow-3xl group-hover:rotate-2"
           />
         </div>
 
-        <div className = "absolute top-[30%] left-[62%] z-25 rotate-6">
+        <div className = "absolute top-[30%] left-[62%] z-25 rotate-6 group">
           <Image
             src="/cars.png"
             alt="picture of cars event chicago"
             width={400}
             height={200}
-            className="rounded-[40px] shadow-2xl"
+            className="rounded-[40px] shadow-2xl transition-transform duration-300 ease-out hover:scale-110 hover:-translate-y-2 hover:shadow-3xl group-hover:rotate-2"
           />
         </div>
 
-        <div className="absolute top-[17%] left-[15%] z-10 ">
+        <div className="absolute top-[17%] left-[15%] z-10 group">
           <Image
             src="/sparkHacks.png"
             alt="picture of me at sparkHacks"
             width={270}
             height={200}
-            className="rounded-2xl"
+            className="rounded-2xl transition-transform duration-300 ease-out hover:scale-110 hover:-translate-y-2 hover:shadow-3xl group-hover:rotate-2"
           />
         </div>
 
         <div className="absolute top-[65%] -left-66 w-full z-50 text-center">
-          <p className="text-5xl font-bold font-sans text-yellow-600">
+          <p className="text-5xl font-bold font-sans text-yellow-600 tracking-wider drop-shadow-lg [text-shadow:1px_1px_2px_rgba(0,0,0,0.5)]">
             <span className="tracking-[0.13em]">Student.Mentor.</span><br />
             Developer.Engineer.
           </p>
